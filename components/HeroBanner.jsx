912b7c3ef12726d577207578101af09b0275af96
RaulDev99 <raulskate1999@gmail.com>
2022-06-13 09:49:32 +0200

    update

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -5,28 +5,35 @@ import {urlFor} from '../lib/client'
 
 const HeroBanner = ({heroBanner:{smallText,saleTime,image,product,buttonText,desc}}) => {
   return (
+    
+    
     <div className="hero-banner-container">
-      <div>
-        <p className="beats-solo">{smallText}</p>
-      </div>
-
-      <h3>{saleTime}</h3>
-
-      <img src={urlFor(image)} alt="tesoros" className="hero-banner-image"/>
-
-      <div>
-        <Link href={`/product/${product}`}>
-          <button type="button">{buttonText}</button>
-        </Link>
-        <div className="desc">
-          <h5>Description</h5>
-          <p>{desc}</p>
-
+      
+        <img src={urlFor(image)} alt="tesoros" className="hero-banner-image"/>
+      
+      <div className="hero-banner-heading">
+        
+        <h3>{saleTime}</h3>
+        <div>
+          <p className="beats-solo">{smallText}</p>
         </div>
+        <div>
+          <Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
+          </Link>
+          
+        </div>
+        
       </div>
-
+      <div className="desc">
+            <h5>Description</h5>
+            <p>{desc}</p>
+        </div>
+      
+      
 
     </div>
+    
   )
 }
 
